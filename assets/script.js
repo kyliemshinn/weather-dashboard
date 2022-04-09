03694e0a04e13d78d9e8a0763deead9cad0c54d8
kylie <kyliemshinn@gmail.com>
2022-04-09 15:22:43 -0700

    create outline of HTML and CSS

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+function currentWeatherConditons(){
+//localstorage to add and append to page the current search history
+}
+
+function futureWeatherConditions() {
+
+}
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+function cityMetrics() {
+
+}
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+function uvIndex() {
+    //change colors depending on the number of uv index- red is very hot, blue is no bad
+}
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
+function weatherForecast() {
+    
+}
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+function searchHistory() {
+
+}
+
+
+//will need to use geotag and open api on open weather app
