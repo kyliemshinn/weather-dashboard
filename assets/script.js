caee821000d935e93dc87e06d20c30132218ff21
kylie <kyliemshinn@gmail.com>
2022-04-10 09:07:57 -0700

    create javascript outline

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,40 @@
+//Acceptance Criteria
 
+// GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
-function currentWeatherConditons(){
-//localstorage to add and append to page the current search history
-}
-
-function futureWeatherConditions() {
-
-}
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-function cityMetrics() {
-
-}
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-function uvIndex() {
-    //change colors depending on the number of uv index- red is very hot, blue is no bad
-}
 // WHEN I view future weather conditions for that city
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-function weatherForecast() {
-    
-}
 // WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-function searchHistory() {
-
-}
 
 
-//will need to use geotag and open api on open weather app
+//function search for a city and populate the  weather
+    //show current weather
+    //show future weather- 5 day forecast
+    //append to page under the search tab
+
+//current weather conditons function
+    //present city name-larger than rest
+    //show todays date
+    //weather icon representing the current weather(ex. cloud emoji for cloudy days)
+    // the temperature 
+    //the humidity
+    //the wind speed
+    //UV index
+
+//uv index color change
+    //change color- blue- for low index, yellow- for moderate index, red- for high index(set properties in CSS)
+
+//five day forecast
+    //show date
+    //show weather icon
+    //show temperature
+    //show humidity
+
+//click search history and will populate the information again
+
+
